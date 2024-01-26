a24d988c4d01dd8176260e9b14752d30274e1308
Pham Quoc Viet <vietpqhe173451@fpt.edu.vn>
2024-01-26 19:08:22 +0700

    Update html/css/js file for login page

--- src/main/resources/static/Js/Login.js
+++ src/main/resources/static/Js/Login.js
@@ -4,8 +4,10 @@ const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
+  document.title = "Sign up"
 });
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+  document.title = "Log in"
 });
